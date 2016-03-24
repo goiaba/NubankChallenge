4ff8ed3950b11bd7264fc3b6461d32e6905e9fb4
Bruno G M Correa <brunogmc@gmail.com>
2016-03-24 13:47:19 -0500

    Documenting and improving the code. Nothing new but a more readable code.

--- src/main/scala/br/com/goiaba/core/model/InvitationTree.scala
+++ src/main/scala/br/com/goiaba/core/model/InvitationTree.scala
@@ -12,7 +12,7 @@ case class InvitationTree(key: Int, data: Data = Data(), children: Set[Invitatio
 
   val scoreMultiplier = 0.5
 
-  def find(key: Int): Option[InvitationTree] = {
+  override def find(key: Int): Option[InvitationTree] = {
     @tailrec def findRec(trees: List[InvitationTree]): Option[InvitationTree] = trees match {
       case Nil => None
       case (h :: t) =>
@@ -22,42 +22,38 @@ case class InvitationTree(key: Int, data: Data = Data(), children: Set[Invitatio
     findRec(List(this))
   }
 
-  def pathTo(key: Int): List[InvitationTree] = {
-    def findPathRec(currTree: InvitationTree, currPath: List[InvitationTree]): List[InvitationTree] = {
+  override def pathTo(key: Int): List[InvitationTree] = {
+    def pathToRec(currTree: InvitationTree, currPath: List[InvitationTree]): List[InvitationTree] = {
       if (key == currTree.key) currTree :: currPath
-      else currTree.children.toList.flatMap(findPathRec(_, currTree :: currPath))
+      else currTree.children.map(pathToRec(_, currTree :: currPath)).toList.flatten
     }
-    findPathRec(this, List())
+    pathToRec(this, List())
   }
 
-  override def insert(key: Int, parentKey: Int): InvitationTree = find(parentKey) match {
+  /*
+   * When inserting, the following rules will be applied:
+   *
+   * p: the key to be inserted already exists in the tree
+   * q: the tree represented by parentKey has invited before
+   *
+   *  p &&  q => Do nothing
+   *  p && !q => Update the trees from parent up to the root
+   * !p &&  q => Insert the new tree in the parent's children list.
+   * !p && !q => Insert and Update
+   *
+   * The 'data' argument is ignored, since all the information it
+   * could carry are calculated here.
+   */
+  override def insert(key: Int, data: Data, parentKey: Int): InvitationTree = find(parentKey) match {
     case None => throw new RuntimeException("parentKey [" + parentKey + "] does not exist.")
-    case Some(parentTree) => {
-      val pathToParent = pathTo(parentKey)
-      pathToParent match {
-        case Nil => throw new RuntimeException("Unexpected behavior on findPath method. Path to parent not found.")
-        case (h :: t) => {
-          val z = find(key) match {
-            case None => parentTree.copy(data = parentTree.data.copy(hasInvitedBefore = true),
-              children = parentTree.children + InvitationTree(key))
-            case Some(existingTree) => parentTree.copy(data = parentTree.data.copy(hasInvitedBefore = true))
-          }
-          t.foldLeft(z)((acc, item) => {
-            val indexOfCurrentItem = pathToParent.indexOf(item)
-            val priorOutdatedItem = pathToParent.lift(indexOfCurrentItem-1).get
-            InvitationTree(item.key,
-              scoreFunction(item.data, parentTree.data.hasInvitedBefore, indexOfCurrentItem-1),
-              item.children.&~(Set(priorOutdatedItem)) + acc)
-          })
-        }
-      }
+    case Some(parentTree) => find(key) match {
+      case Some(tree) if parentTree.data.hasInvitedBefore => this
+      case Some(tree) => updateTree(parentTree)
+      case None => updateTree(parentTree, Option(key))
     }
   }
 
-  protected def scoreFunction(data: Data, hasInvitedBefore: Boolean, level: Int): Data = hasInvitedBefore match {
-    case true => data
-    case false => Data(data.score + math.pow(scoreMultiplier, level), true)
-  }
+  def insert(key: Int, parentKey: Int): InvitationTree = insert(key, Data(), parentKey)
 
   def ranking: List[(Int, Double)] = {
     @tailrec def rankingRec(trees: List[InvitationTree], l: List[(Int, Double)]): List[(Int, Double)] = trees match {
@@ -68,6 +64,39 @@ case class InvitationTree(key: Int, data: Data = Data(), children: Set[Invitatio
       }
     }
     rankingRec(List(this), List()).sortWith(_._2 > _._2)
+  }
+
+  protected def updateData(data: Data, hasInvitedBefore: Boolean, level: Int): Data = hasInvitedBefore match {
+    case true => data
+    case false => Data(data.score + math.pow(scoreMultiplier, level), true)
+  }
+
+  protected def updateTree(parentTree: InvitationTree, key: Option[Int] = None): InvitationTree = {
+    val newParentTree = key match {
+      case None => parentTree.copy(data = parentTree.data.copy(hasInvitedBefore = true))
+      case Some(k) => parentTree.copy(data = parentTree.data.copy(hasInvitedBefore = true),
+    children = parentTree.children + InvitationTree(k))
+    }
+    val pathToParent = pathTo(parentTree.key)
+    //pathToParent is guaranteed to be a List so we can call tail
+    pathToParent.tail.foldLeft(newParentTree)((acc, item) => {
+      //since we are here, inside the foldLeft, and considering it was applied over
+      // the tail of pathToParent, we are guaranteed that there is a prior item (head).
+      //
+      // indexOfPriorItem will be used to retrieve the outdated InvitationTree,
+      // priorOutdatedItem, that must be removed from the children's list of the
+      // current item. After this removal, a new InvitationTree will be generated
+      // with its children's list consisting of:
+      //   item.children diff Set(priorOutdatedItem) + acc
+      // where acc represents the InvitationTree created in the previous step of foldLeft.
+      //
+      // indexOfPriorItem will also be used as the level to update the score of the item
+      val indexOfPriorItem = pathToParent.indexOf(item)-1
+      val priorOutdatedItem = pathToParent.lift(indexOfPriorItem).get
+      InvitationTree(item.key,
+        updateData(item.data, parentTree.data.hasInvitedBefore, indexOfPriorItem),
+        (item.children diff Set(priorOutdatedItem)) + acc)
+    })
   }
 
 }
